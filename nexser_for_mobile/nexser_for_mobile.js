8bed2b86c6f1eabad22943179d5293072c0415c7
moti5768 <103321115+moti5768@users.noreply.github.com>
2024-03-09 21:16:12 +0900

    Update nexser_for_mobile.js

--- nexser_for_mobile/nexser_for_mobile.js
+++ nexser_for_mobile/nexser_for_mobile.js
@@ -2267,38 +2267,62 @@ if (!(ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android'
 
 
     document.querySelectorAll('.drag_button2').forEach(function (drag) {
-        drag.addEventListener('mousedown', function () {
-
-            let drag2 = drag.closest('#toolbar');
-            drag2.classList.add('drag');
-
-            draggable(document.querySelector('.drag'));
-            function draggable(drag) {
-                drag2.onmousedown = function (event) {
-                    let shiftX = event.clientX - drag.getBoundingClientRect().left;
-                    let shiftY = event.clientY - drag.getBoundingClientRect().top;
-                    let top = document.querySelector('.top');
-                    moveAt(event.pageX, event.pageY);
-                    // ボールを（pageX、pageY）座標の中心に置く
-                    function moveAt(pageX, pageY) {
-                        drag.style.left = pageX - shiftX + 'px';
-                        drag.style.top = pageY - shiftY + 'px';
-                    }
-                    function onMouseMove(event) {
-                        moveAt(event.pageX, event.pageY);
-                    }
-                    // (3) mousemove でボールを移動する
-                    document.addEventListener('mousemove', onMouseMove);
-                    // (4) ボールをドロップする。不要なハンドラを削除する
-                    drag.onmouseup = function () {
-                        document.removeEventListener('mousemove', onMouseMove);
-                        drag.onmouseup = null;
-                        drag2.classList.remove('drag');
-                        draggable()
-                    };
-                };
-            };
-        });
+        let drag2 = drag.closest('#toolbar');
+        //要素内のクリックされた位置を取得するグローバル（のような）変数
+        var x;
+        var y;
+        //マウスが要素内で押されたとき、又はタッチされたとき発火
+        drag.addEventListener("mousedown", mdown, false);
+        drag.addEventListener("touchstart", mdown, false);
+        //マウスが押された際の関数
+        function mdown(e) {
+            //クラス名に .drag を追加
+            drag2.classList.add("drag");
+            //タッチデイベントとマウスのイベントの差異を吸収
+            if (e.type === "mousedown") {
+                var event = e;
+            } else {
+                var event = e.changedTouches[0];
+            }
+            //要素内の相対座標を取得
+            x = event.pageX - drag2.offsetLeft;
+            y = event.pageY - drag2.offsetTop;
+            //ムーブイベントにコールバック
+            document.body.addEventListener("mousemove", mmove, false);
+            document.body.addEventListener("touchmove", mmove, false);
+        }
+        //マウスカーソルが動いたときに発火
+        function mmove(e) {
+            //ドラッグしている要素を取得
+            var drag = document.getElementsByClassName("drag")[0];
+            //同様にマウスとタッチの差異を吸収
+            if (e.type === "mousemove") {
+                var event = e;
+            } else {
+                var event = e.changedTouches[0];
+            }
+            //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+            e.preventDefault();
+            //マウスが動いた場所に要素を動かす
+            drag.style.top = event.pageY - y + "px";
+            drag.style.left = event.pageX - x + "px";
+            //マウスボタンが離されたとき、またはカーソルが外れたとき発火
+            drag.addEventListener("mouseup", mup, false);
+            document.body.addEventListener("mouseleave", mup, false);
+            drag.addEventListener("touchend", mup, false);
+            document.body.addEventListener("touchleave", mup, false);
+        }
+        //マウスボタンが上がったら発火
+        function mup(e) {
+            var drag = document.getElementsByClassName("drag")[0];
+            //ムーブベントハンドラの消去
+            document.body.removeEventListener("mousemove", mmove, false);
+            drag.removeEventListener("mouseup", mup, false);
+            document.body.removeEventListener("touchmove", mmove, false);
+            drag.removeEventListener("touchend", mup, false);
+            //クラス名 .drag も消す
+            drag.classList.remove("drag");
+        }
     })
 
     document.querySelector('.toolbar_on').addEventListener('click', function () {
